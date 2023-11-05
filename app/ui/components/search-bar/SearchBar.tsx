91a9ccbad9a68dcd4f0b1a96445bb586a009a221
KevC000 <chenkevin1119@gmail.com>
2023-11-05 04:49:53 -0500

    minor spotlight changes

--- app/ui/components/search-bar/SearchBar.tsx
+++ app/ui/components/search-bar/SearchBar.tsx
@@ -3,8 +3,13 @@
 import React from "react";
 
 import { ActionIcon, TextInput, rem } from "@mantine/core";
-import { Spotlight, SpotlightActionData, spotlight } from "@mantine/spotlight";
-import { IconArrowRight, IconSearch } from "@tabler/icons-react";
+import {
+  Spotlight,
+  SpotlightAction,
+  SpotlightActionData,
+  spotlight,
+} from "@mantine/spotlight";
+import { IconArrowRight, IconHome, IconSearch } from "@tabler/icons-react";
 
 import classes from "./SearchBar.module.css";
 
@@ -45,20 +50,25 @@ const SearchBar = (props: Props) => {
           </ActionIcon>
         }
       />
-      <Spotlight
-        actions={actions}
-        nothingFound="Nothing found..."
-        highlightQuery
-        searchProps={{
-          leftSection: (
-            <IconSearch
-              style={{ width: rem(20), height: rem(20) }}
-              stroke={1.5}
-            />
-          ),
-          placeholder: "Search...",
-        }}
-      />
+
+      <Spotlight.Root
+        query={""}
+        onSpotlightOpen={() => {}}
+        onQueryChange={() => {}}
+        closeOnClickOutside
+        closeOnEscape
+        scrollable
+      >
+        <Spotlight.Search
+          placeholder="Search..."
+          rightSection={<IconSearch stroke={1.5} />}
+        />
+
+        <Spotlight.ActionsList>
+          <SpotlightAction label="lol" highlightQuery></SpotlightAction>
+          <Spotlight.Empty>Nothing found...</Spotlight.Empty>
+        </Spotlight.ActionsList>
+      </Spotlight.Root>
     </>
   );
 };
