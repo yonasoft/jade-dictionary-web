7bc30910031a8d19ffa4710c27e188d162fc7ea7
KevC000 <chenkevin1119@gmail.com>
2023-11-05 02:50:25 -0500

    fixed sign up button color

--- app/ui/components/navbar/nav-items/auth-items/AuthButtons.tsx
+++ app/ui/components/navbar/nav-items/auth-items/AuthButtons.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { Button } from "@mantine/core";
 
-import classes from "./AuthButtons.module.css";
+import classes from "./AuthItems.module.css";
 
 type Props = {};
 
