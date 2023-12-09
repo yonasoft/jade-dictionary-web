efb5d0b9e66bb3c5c27774adfdce2571b202301e
KevC000 <chenkevin1119@gmail.com>
2023-12-09 12:59:51 -0500

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Container } from "@mantine/core";
 import Image from "next/image";
 
 const Home = () => {
-  return <h1>hello world</h1>;
+  return (
+    <Container size="lg">
+      <h1>hello world</h1>
+    </Container>
+  );
 };
 
 export default Home;
