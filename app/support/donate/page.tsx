ee7a82923224d97dac384dd6725131b18d267159
KevC000 <chenkevin1119@gmail.com>
2023-12-13 15:18:39 -0500

    donation page

--- app/support/donate/page.tsx
+++ app/support/donate/page.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Text, Button, Container, Anchor, Title, Center } from "@mantine/core";
 
-type Props = {};
+const Donate = () => {
+  return (
+    <>
+      <Center>
+        <Title>谢谢！</Title>
+      </Center>
+      <div className="flex flex-col items-center justify-center">
+        <Text size="lg" className="mb-4 text-center">
+          Enjoying Jade and want to support us? Consider donating!
+        </Text>
+        <Button
+          variant="filled"
+          component="a"
+          href="https://www.buymeacoffee.com/yonasoft"
+          target="_blank"
+          className="mb-4"
+        >
+          Buy Me A Coffee
+        </Button>
 
-const Donate = (props: Props) => {
-  return <div>donate</div>;
+        <Button
+          variant="filled"
+          component="a"
+          href="https://ko-fi.com/yonasoft"
+          target="_blank"
+          className="mb-4"
+        >
+          Ko-Fi
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Donate;
