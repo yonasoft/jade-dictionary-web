fc8b8a43ad4638102adf19f1023996929f1a58e8
KevC000 <chenkevin1119@gmail.com>
2023-12-13 15:25:55 -0500

    donation setup and initial contact us

--- app/support/contact/page.tsx
+++ app/support/contact/page.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { TextInput, Textarea, Button, Text } from "@mantine/core";
 
-type Props = {};
+const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [topic, setTopic] = useState("");
+  const [message, setMessage] = useState("");
 
-const Contact = (props: Props) => {
-  return <div>contact</div>;
+  const handleSubmit = async () => {
+    // Logic to send email. You'll need to integrate with an email sending service.
+    console.log("Sending Email:", { email, topic, message });
+
+    // Reset form after sending email
+    setEmail("");
+    setTopic("");
+    setMessage("");
+  };
+
+  return (
+    <div className="mt-10">
+      <Text>dfdf</Text>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          required
+          label="Your Email"
+          placeholder="your.email@example.com"
+          value={email}
+          onChange={(event) => setEmail(event.currentTarget.value)}
+        />
+        <TextInput
+          required
+          label="Topic"
+          placeholder="Help with Jade"
+          value={topic}
+          onChange={(event) => setTopic(event.currentTarget.value)}
+        />
+
+        <Textarea
+          required
+          label="Message"
+          placeholder="Your message"
+          value={message}
+          onChange={(event) => setMessage(event.currentTarget.value)}
+        />
+        <Button type="submit" className="mt-4">
+          Send Message
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
