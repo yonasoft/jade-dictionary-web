d10e6bfc6b6e5508a2bbeb821b96799df1133d93
KevC000 <chenkevin1119@gmail.com>
2023-12-14 01:22:54 -0500

    public key

--- app/lib/utils/emailjs.ts
+++ app/lib/utils/emailjs.ts
@@ -6,22 +6,18 @@ export const sendEmail = (e:React.FormEvent<HTMLFormElement>) => {
 
 	console.log('Service ID:', process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
 	console.log('Template ID:', process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID);
-	console.log('Public Key:', process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY);
+	console.log('Public Key:', process.env.NEXT_PUBLIC_EMAILJS_KEY);
 
 
 	emailjs.sendForm(
 	process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string,
 	process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string,
 	e.target as HTMLFormElement,
-	process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+	process.env.NEXT_PUBLIC_EMAILJS_KEY
 	)
 	.then((result) => {
 		console.log(result.text);
 	}, (error) => {
 		console.error(error.text);
-		console.log(
-			process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string,
-			process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string,
-			process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string)
 	});
 };
