2658027d37c028a64461a9ceb57c2c8bf2b66694
KevC000 <chenkevin1119@gmail.com>
2023-12-14 01:06:10 -0500

    emailjs console log

--- app/lib/utils/emailjs.ts
+++ app/lib/utils/emailjs.ts
@@ -4,6 +4,11 @@ import emailjs from 'emailjs-com';
 export const sendEmail = (e:React.FormEvent<HTMLFormElement>) => {
   	e.preventDefault();
 
+	console.log('Service ID:', process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
+	console.log('Template ID:', process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID);
+	console.log('Public Key:', process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY);
+
+
 	emailjs.sendForm(
 	process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string,
 	process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string,
