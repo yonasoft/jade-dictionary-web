ce0bf8612b175e4cc706df6a553bd5b2bc05fe65
KevC000 <chenkevin1119@gmail.com>
2023-12-17 21:25:14 -0500

    home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import { Suspense, lazy, memo, useMemo, useState } from "react";
 import {
   BackgroundImage,
   Box,
+  Card,
   Center,
   Container,
   Flex,
+  Grid,
+  Paper,
   Text,
   Title,
   useMantineColorScheme,
@@ -49,33 +52,39 @@ const Home = () => {
   const theme = useMantineTheme();
   const [query, setQuery] = useState("");
 
-  const titleColor = useMemo(
-    () => (colorScheme === "dark" ? theme.colors.dark[9] : theme.white),
-    [colorScheme, theme]
-  );
-
   return (
-    <Container size="lg" className="relative">
-      <Box h={250}>
-        <BackgroundImage src="/image/jade-background.jpg" radius="lg" h={250}>
-          <Center p="md">
-            <Flex direction="column">
-              <Title c={titleColor}>Jade English-Chinese Dictionary</Title>
-              <Center className="mt-5">
-                <SearchBar
-                  outsideQuery={query}
-                  outsideSetQuery={setQuery}
-                  openSpotlight={false}
-                />
-              </Center>
-            </Flex>
-          </Center>
+    <Container size="lg" className="my-8 mx-auto">
+      <Paper shadow="md" p="lg" radius="md" className="mb-8">
+        <BackgroundImage src="/image/jade-background.jpg" radius="lg">
+          <Flex direction="column" className="p-8 text-center">
+            <Title
+              className={`text-${colorScheme === "dark" ? "white" : "black"}`}
+            >
+              Jade English-Chinese Dictionary
+            </Title>
+            <Center className="mt-5">
+              <SearchBar
+                outsideQuery={query}
+                outsideSetQuery={setQuery}
+                openSpotlight={false}
+              />
+            </Center>
+          </Flex>
         </BackgroundImage>
-      </Box>
+      </Paper>
+
       {loading ? (
         <Loading />
       ) : results.length > 0 ? (
-        <Results results={results} query={query} />
+        <Grid>
+          {results.map((word, index) => (
+            <Grid.Col key={index} span={{ base: 12, sm: 6, lg: 4 }}>
+              <Suspense fallback={<Loading />}>
+                <WordResult word={word} query={query} />
+              </Suspense>
+            </Grid.Col>
+          ))}
+        </Grid>
       ) : (
         <NothingFound />
       )}
