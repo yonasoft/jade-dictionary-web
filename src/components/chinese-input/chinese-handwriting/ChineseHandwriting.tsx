f7c312e7c66ec40e8ad905f27d0781ed4914aa07
KevC000 <chenkevin1119@gmail.com>
2024-01-22 20:03:12 -0500

    adjusted delay on handwritign stroke

--- src/components/chinese-input/chinese-handwriting/ChineseHandwriting.tsx
+++ src/components/chinese-input/chinese-handwriting/ChineseHandwriting.tsx
@@ -134,7 +134,7 @@ const ChineseHandwriting = ({ query, setQuery }: Props) => {
         const newPath: HanziStroke = [...lastPath, point];
         return [...prevPaths.slice(0, -1), newPath];
       });
-    }, 5),
+    }, 7),
     []
   );
 
