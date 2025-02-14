462c14fac4c730607d7df807829fb10d798dd57d
Kevin Chen <yonasoft7@gmail.com>
2025-02-14 15:30:28 -0500

    upgraded next version and added vervel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Head from "next/head";
 import { ReactNode, FC } from "react";
-
 import { MantineProvider, Space } from "@mantine/core";
 import { ContextModalProps, ModalsProvider } from "@mantine/modals";
-
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "@mantine/core/styles.css";
 import "./globals.css";
 import AddWordListModal from "../components/modals/add-word-list-modal/AddWordListModal";
@@ -39,36 +39,38 @@ const modals: Record<string, FC<ContextModalProps<any>>> = {
 
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <link
-          rel="icon"
-          href="/image/jadeicon.ico"
-          type="image/icon"
-          sizes="<generated>"
+          rel='icon'
+          href='/image/jadeicon.ico'
+          type='image/icon'
+          sizes='<generated>'
         />
         <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
         />
-        <meta charSet="UTF-8" />
-        <meta name="description" content={metadata.description as string} />
+        <meta charSet='UTF-8' />
+        <meta name='description' content={metadata.description as string} />
         <title>{metadata.title as string}</title>
       </head>
       <body
         className={`${roboto.className} antialiased`}
         suppressHydrationWarning={true}
       >
-        <MantineProvider theme={theme} defaultColorScheme="dark">
+        <MantineProvider theme={theme} defaultColorScheme='dark'>
           <FirebaseContextProvider>
             <ModalsProvider modals={modals}>
               <Navbar />
-              <Space h="md" />
+              <Space h='md' />
               <main>{children}</main>
-              <div id="chinese-input-root"></div>
+              <div id='chinese-input-root'></div>
             </ModalsProvider>
           </FirebaseContextProvider>
         </MantineProvider>
+        <Analytics />
+         <SpeedInsights />
       </body>
     </html>
   );
